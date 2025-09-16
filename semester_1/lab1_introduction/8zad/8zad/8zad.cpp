719a7c71f9815b74ac3d5dd20c91cd8fb373082b
lll <artvolk07@gmail.com>
2025-09-16 17:21:49 +0300

    finally

---
+++ semester_1/lab1_introduction/8zad/8zad/8zad.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    const int n = 10;
+    double numbers[n];
+
+    std::cout << "Vvedi 10 chisel: " << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cin >> numbers[i];
+    }
+
+    int C = 0;
+
+    for (int i = 1; i < n - 1; i++) {
+        if (numbers[i] < numbers[i - 1] && numbers[i] < numbers[i + 1]) {
+            C++;
+        }
+    }
+
+    std::cout << "Kol vo < sosedei: " << C << std::endl;
+
+    return 0;
+}
+
