591c8c3b5c5cf86ee41a775c981c3f32061e054e
lll <artvolk07@gmail.com>
2025-12-22 20:17:34 +0300

    ff

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,43 +1,40 @@
-#ifndef VECTOR_H
-#define VECTOR_H
+    #pragma once
 
-#include <initializer_list>
-#include <stdexcept>
-#include <ostream>
+    #include <initializer_list>
+    #include <stdexcept>
+    #include <ostream>
 
-class Vector {
-public:
-    Vector();
-    explicit Vector(unsigned int n);
-    Vector(std::initializer_list<int> init);
-    Vector(const Vector& other);
-    Vector& operator=(const Vector& other);
-    ~Vector();
+    class Vector {
+    public:
+        Vector();
+        explicit Vector(unsigned int n);
+        Vector(std::initializer_list<int> init);
+        Vector(const Vector& other);
+        Vector& operator=(const Vector& other);
+        ~Vector();
 
-    void Swap(Vector& other);
+        void Swap(Vector& other);
 
-    int& operator[](unsigned int index);
-    const int& operator[](unsigned int index) const;
+        int& operator[](unsigned int index);
+        const int& operator[](unsigned int index) const;
 
-    int& At(unsigned int index);
-    const int& At(unsigned int index) const;
+        int& At(unsigned int index);
+        const int& At(unsigned int index) const;
 
-    unsigned int Size() const;
-    unsigned int Capacity() const;
+        unsigned int Size() const;
+        unsigned int Capacity() const;
 
-    void PushBack(int value);
-    void PopBack();
-    void Clear();
-    void Reserve(unsigned int new_capacity);
+        void PushBack(int value);
+        void PopBack();
+        void Clear();
+        void Reserve(unsigned int new_capacity);
 
-private:
-    int* data_;
-    unsigned int size_;
-    unsigned int capacity_;
+    private:
+        int* data_;
+        unsigned int size_;
+        unsigned int capacity_;
 
-    void ReallocateTo(unsigned int new_capacity);
-};
+        void ReallocateTo(unsigned int new_capacity);
+    };
 
-std::ostream& operator<<(std::ostream& os, const Vector& v);
-
-#endif
+    std::ostream& operator<<(std::ostream& os, const Vector& v);
